61abd32d47d01ec1ab8a1e52b56d0855cbd7859d
root <andrewseigner@gmail.com>
2010-04-23 19:59:57 -0700

    fixed routes syntax

--- routes.rb
+++ routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect 'geo_bounds', :controller => 'geobounds', :action => 'calculate'
 
-  map.root :controller => "geobounds", :action => 'calculate
+  map.root :controller => "geobounds", :action => 'calculate'
 end
